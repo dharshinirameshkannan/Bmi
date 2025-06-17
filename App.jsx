895153c798adb88aa6761d28dec48b09cf73e8f1
dharshinirameshkannan <dharshinirameshkannan1@gmail.com>
2025-06-17 07:56:57 -0700

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BmiCalculator from "./Components/BmiCalculator";
+
+const App = () => {
+  return (
+    <div>
+      <BmiCalculator />
+    </div>
+  );
+};
+
+export default App;
